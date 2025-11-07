790945ed4321772e0bd4528583ae61bfa1137327
Gabriel Coimbra <gabao.coimbra@gmail.com>
2025-11-07 07:30:02 -0300

    Configurando Constantes de utilização para base url e cores

---
+++ lib/constants/colors.constants.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ColorsConstants {
+  ColorsConstants._();
+
+  static const Color primary = Color(0xFF000000);
+  static const Color secondary = Color.fromARGB(255, 32, 145, 238);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color surface = Color(0xFFE0E0E0);
+  static const Color textSecondary = Color.fromARGB(255, 83, 83, 83);
+}
---
+++ lib/constants/urls.constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  ApiConstants._();
+  static const String _baseUrl = 'https://rickandmortyapi.com/';
+  static const String api = '$_baseUrl/api';
+}
